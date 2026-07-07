e47c431c7d06e01c59b9f9fcf795bb89fee2f148
EclipseaHime017 <12210226@mail.sustech.edu.cn>
2026-07-08 05:00:17 +0800

    start.sh中新增指令增加buffer预留长度

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 for can in can0 can1 can2 can3; do
     sudo ip link set "$can" down 2>/dev/null || true
     sudo ip link set "$can" up type can bitrate 1000000 restart-ms 10 2>/dev/null || true
+    sudo ip link set "$can" txqueuelen 1000
 done
